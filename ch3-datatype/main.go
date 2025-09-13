1646080ba9bffd4be7d60e655ba1b177de96af16
TwoTanawin <tanawinsiriwan@outlook.com>
2025-09-13 12:18:43 +0700

    ch3-datatype

---
+++ ch3-datatype/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int8 = 10
+	fmt.Println("a : ", a)
+
+	var k float32 = 1.1
+	fmt.Println("k : ", k)
+
+	var m string = "Hello"
+	fmt.Println("m : ", m)
+
+	var n bool = true
+	fmt.Println("n : ", n)
+
+}
